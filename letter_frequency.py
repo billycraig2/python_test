7ac51b44705169dae3e19ead20b55c239cc9f583
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-05 15:35:27 +0530

    script that uses dictionary to print frequency of letters in a string

---
+++ letter_frequency.py
@@ -0,0 +1,15 @@
+message = input("Input: ")
+
+message_dict = {}
+
+# looping through each letter of the input 
+for letter in message:
+    # condition to check if the letter already exists in dict
+    if letter in message_dict.keys():
+        message_dict[letter] = message_dict[letter] + 1
+    else:
+        message_dict[letter] = 1
+
+# printing the frequency of each letter in the message
+for key in message_dict:
+    print("Frequency of " + key + " is " + str(message_dict[key]))
