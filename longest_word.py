e694a9b069ce828c60100032afcccd09a3f38668
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-05 16:54:19 +0530

    script that looks for the longest word in a message

---
+++ longest_word.py
@@ -0,0 +1,16 @@
+message = input('Input: ')
+longest_word = ''
+current_word = ''
+
+# loop through each letter of the input message
+for letter in message+' ':
+    # condition to check if alphabet letter
+    if letter.isalpha():
+        current_word = current_word + letter
+    else:
+        # condition to check if the current word is longer than longest word
+        if len(current_word) > len(longest_word):
+            longest_word = current_word
+        current_word = ''
+
+print (longest_word)
