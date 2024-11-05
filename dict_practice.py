e45c57a5803a7afbbe9204c4d2a2e6b48d3fb014
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-05 11:49:37 +0530

    2 exercises with basic dictionaries

--- dict_practice.py
+++ dict_practice.py
@@ -1,8 +1,42 @@
-my_dict = {'name': 'billy', 'age': 22, 'city': 'valencia'}
-print(my_dict)
+'''
+EXERCISE 1 - BASIC OPERATIONS
 
-my_dict['email'] = 'billy2002craig@gmail.com'
-print(my_dict)
+my_book = {'title': 'harry potter', 'year': 1999, 'author': 'jk rowling', 'genre': 'fantasy'}
+print(my_book)
 
-del my_dict['email']
-print(my_dict)
+my_book['rating'] = '5*'
+print(my_book)
+
+my_book['year'] = 1998
+print(my_book)
+
+del my_book['rating']
+print(my_book)
+
+title = my_book.get('title')
+items = my_book.items()
+print(title)
+print(items)
+'''
+
+'''
+EXERCISE 2 - NESTED DICTIONARY
+
+student = {
+    'name': 'billy',
+    'age': 22,
+    'courses': {
+        'tech': 'A',
+        'business': 'A',
+        'it': 'A',
+    }
+}
+print(student)
+
+student['courses']['chem'] = 'A'
+print(student)
+
+student['courses']['chem'] = 'C'
+print(student)
+'''
+
