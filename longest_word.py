b73c6f9b754883f9fdc76716c64d7a281737876d
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-06 11:44:13 +0530

    defined find_longest_word function for reusability

--- longest_word.py
+++ longest_word.py
@@ -1,16 +1,20 @@
-message = input('Input: ')
-longest_word = ''
-current_word = ''
+def find_longest_word(message):
+    longest_word = ''
+    current_word = ''
 
-# loop through each letter of the input message
-for letter in message+' ':
+    # loop through each letter of the input message
+    for letter in message+' ':
     # condition to check if alphabet letter
-    if letter.isalpha():
-        current_word = current_word + letter
-    else:
-        # condition to check if the current word is longer than longest word
-        if len(current_word) > len(longest_word):
-            longest_word = current_word
-        current_word = ''
+        if letter.isalpha():
+            current_word = current_word + letter
+        else:
+            # condition to check if the current word is longer than longest word
+            if len(current_word) > len(longest_word):
+                longest_word = current_word
+            current_word = ''
 
-print (longest_word)
+    print (longest_word)
+        
+    
+message = input('Input: ')
+find_longest_word(message)
