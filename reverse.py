1d58e8ba21e37b36d2e445873a1dae0690b905ff
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-12 17:07:25 +0530

    added logic into a function

--- reverse.py
+++ reverse.py
@@ -1,8 +1,11 @@
+def reverse_list(input):
+    # go through each element and swap to position
+    for i in range(len(input)//2):
+        j = len(input) - 1 - i
+        input[i], input[j] = input[j], input[i]
+
+    return input
+
+
 input = [1, 2, 3, 4, 5, 6, 7]
-
-# go through each element and swap to position
-for i in range(len(input)//2):
-    j = len(input) - 1 - i
-    input[i], input[j] = input[j], input[i]
-
-print(input)
+print(reverse_list(input))
