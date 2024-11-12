ec8cfe6fccd457aa2961361968ae0f4557e10b37
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-12 15:39:42 +0530

    commited wrong version

--- sort.py
+++ sort.py
@@ -1,8 +1,8 @@
 input = [1,2,3,2,2,2,3,1,1]
 
 # go through each element if the element to the right is bigger it swaps them
-for i in input:
-    for j in input:
+for i in range(len(input)):
+    for j in range(len(input)):
         if input[i] < input[j]:
             input[j], input[i] = input[i], input[j]
         else:
