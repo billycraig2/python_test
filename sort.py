05493293e839af5d1601c8ff0027ffd64367383b
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2024-11-12 15:43:54 +0530

    uses bubble sort

--- sort.py
+++ sort.py
@@ -2,9 +2,9 @@ input = [1,2,3,2,2,2,3,1,1]
 
 # go through each element if the element to the right is bigger it swaps them
 for i in range(len(input)):
-    for j in range(len(input)):
-        if input[i] < input[j]:
-            input[j], input[i] = input[i], input[j]
+    for j in range(len(input)-i-1):
+        if input[j] > input[j+1]:
+            input[j], input[j+1] = input[j+1], input[j]
         else:
             continue
 
